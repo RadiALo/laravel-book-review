3bf02923612d39e1af119135ba8f341243578c17
RadiALo <78644882+RadiALo@users.noreply.github.com>
2024-02-04 18:22:41 +0200

    Adding reviews with limitation

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -89,7 +89,7 @@ class Book extends Model
 
     protected static function booted()
     {
-        static::updated(fn(Book $book) => cache()->forget('book:' . $book->id) );
+        static::updated(fn(Book $book) => cache()->forget('book:' . $book->id));
         static::deleted(fn(Book $book) => cache()->forget('book:' . $book->id));
     }
 }
